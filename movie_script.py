8d713d9a4d7977e776eed769b697965a203dcca2
Sahil9229 <sahil.bisht2018@vitstudent.ac.in>
2019-11-24 12:16:03 +0530

    initial commit

---
+++ movie_script.py
@@ -0,0 +1,14 @@
+import imdb
+hr=imdb.IMDb()
+movie_name=input('Enter the movie name : ')
+print()
+movies=hr.search_movie(str(movie_name))
+index=movies[0].getID()
+movie=hr.get_movie(index)
+title=movie['title']
+year=movie['year']
+cast=movie['cast']
+cast_list=','.join(map(str,cast))
+print('Title : ',title)
+print('Year :',year)
+print('Full Cast : ',cast_list)
